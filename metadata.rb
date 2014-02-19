7ed297a4977536eb1df2e5c62184f938368abe88
Darron Froese <darron@froese.org>
2014-02-19 14:27:17 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'mod_pagespeed'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'mod_pagespeed::default', 'Installs/configures something'
